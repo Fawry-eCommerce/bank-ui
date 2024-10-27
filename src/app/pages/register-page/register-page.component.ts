ee4b03af72e673c5dd65649aa987b2ca5efb5fa1
Ziad Ahmed Eliwa <92695464+Ziadeliwa001@users.noreply.github.com>
2024-10-27 21:44:11 +0300

    feat: add regsiter service

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BankService } from '../../services/bank.service';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
+
+  form!: FormGroup;
+
+  constructor(private bankService: BankService) {}
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    });
+  }
+
+  register(): void {
+    if (this.form.valid) {
+      let data = {
+        name: this.form.get('name')?.value,
+        password: this.form.get('password')?.value,
+      }
+      this.bankService.register(data).subscribe((response) => {
+        console.log(response); // TODO: Handle login. ex: goto dashboard
+      });
+    }
+  }
 
 }
